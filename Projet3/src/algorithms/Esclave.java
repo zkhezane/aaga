5c798833a2a70abbc1ef2229a3ede32f3e7143c3
labibayyoub <labib.ayyoub@gmail.com>
2017-10-12 23:42:35 +0200

    update esclave ( adding function "isVoisinGrey" )

--- Projet3/src/algorithms/Esclave.java
+++ Projet3/src/algorithms/Esclave.java
@@ -2,6 +2,7 @@ package algorithms;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
 import java.util.concurrent.Callable;
 
@@ -11,18 +12,26 @@ public class Esclave implements Callable<ArrayList<Point>> {
 	private int edge;
 	private int escNumber;
 	
+	ArrayList<ColoredPoint> allInColor(ArrayList<ColoredPoint> points, ColoredPoint.Color color){
+		for(ColoredPoint p : points)
+			p.setColor(color);
+		return points;
+	}
+
 	public Esclave(ArrayList<Point> points, int edge,int esc) {
 		super();
 		this.points = points;
 		this.edge = edge;
 		this.escNumber=esc;
+		points.removeAll(this.seul(points, edge));
 	}
 
 	@Override
 	public ArrayList<Point> call() throws Exception {
 
 		
-		return this.calculConnectedDominatingSet(points, edge);
+		
+		return this.computeMISValid(points,edge);
 	}
 
 	
@@ -33,28 +42,109 @@ public class Esclave implements Callable<ArrayList<Point>> {
 		  Random r = new Random();
 		  ArrayList<Point> rest = (ArrayList<Point>) points.clone();
 
-		  while(! this.isValidMaximal(points, stable, edgeThreshold)) {
-			  
-
-			  Point pp = rest.get(r.nextInt(rest.size()));
-			  stable.add(pp);
-			  rest.remove(pp);
-			  rest.removeAll(this.voisins(points, pp, edgeThreshold));
-			  
-			  
-		  }
-		  
 
 		  
 		  
 		  System.out.println("Stable : "+stable.size());
 	    return stable;
 	  }
-	  
-	  
 
-	  
-	  public Point plusProche(ArrayList<Point> points,Point p,int edge ) {
+
+	public ArrayList<Point> computeMISValid(ArrayList<Point> points, int edge) {
+		ArrayList<ColoredPoint> whitePoints = new ArrayList<>();
+		
+		for(Point p : points)
+			whitePoints.add(new ColoredPoint(p, ColoredPoint.Color.WHITE));
+		System.out.println("whitePoints "+whitePoints.size());
+		
+	
+		
+		ArrayList<ColoredPoint> MIS = new ArrayList<>();
+		ArrayList<ColoredPoint> listGreys = new ArrayList<>();
+		ArrayList<ColoredPoint> tmpGreys = new ArrayList<>();
+
+		ColoredPoint currentPoint = max(whitePoints, edge);
+		currentPoint.setColor(ColoredPoint.Color.BLACK);
+		MIS.add(currentPoint);
+		System.out.println("MIS: "+ MIS.size());
+		tmpGreys=voisins(whitePoints,currentPoint,edge);	
+		System.out.println("tmpGreys: "+ tmpGreys.size());
+		listGreys.addAll(allInColor(tmpGreys, ColoredPoint.Color.GREY));
+		System.out.println("listGreys: "+ listGreys.size());
+		whitePoints.removeAll(tmpGreys);
+		System.out.println("whitePoints: "+ whitePoints.size());
+		whitePoints.remove(currentPoint);
+		System.out.println("whitePoints: "+ whitePoints.size());
+		int size =0;
+		while(!whitePoints.isEmpty() && whitePoints.size()!= size){
+			size=whitePoints.size();
+			
+			//	System.out.println("grey size :"+listGreys.size());
+			
+//			for(ColoredPoint p : listGreys) {
+//				ArrayList<ColoredPoint> v = this.voisins(listWhite, p, edge);
+//				if(!v.isEmpty())
+//					currentPoint = v.get(0);
+//			}
+			
+			//currentPoint = max(voisinsDeGris(whitePoints, currentPoint, edge),edge);
+			currentPoint=null;
+			for (ColoredPoint p : whitePoints) {
+				if(isVoisinGrey(listGreys, p, edge))
+					currentPoint=p;
+				
+			}
+			if(currentPoint==null)
+				break;
+			currentPoint.setColor(ColoredPoint.Color.BLACK);
+			MIS.add(currentPoint);
+			tmpGreys=voisins(whitePoints,currentPoint,edge);	
+			listGreys.addAll(allInColor(tmpGreys, ColoredPoint.Color.GREY));
+			whitePoints.removeAll(tmpGreys);
+			whitePoints.remove(currentPoint);
+			
+		}
+
+
+		
+		System.out.println("MIS size "+MIS.size());
+
+
+
+
+
+		ArrayList<Point> result = new ArrayList<>();
+		for(Point p : MIS){
+			result.add(new Point(p.x,p.y));
+		}
+		return result;
+	}
+
+	public boolean isVoisinGrey(ArrayList<ColoredPoint> greyPoints,ColoredPoint point,int edge) {
+		
+		for (ColoredPoint p : greyPoints) {
+			if(p.equals(point)) continue;
+			if(p.distance(point)<edge)
+				return true;
+		}
+		return false;
+	}
+	public ArrayList<ColoredPoint> voisinsDeGris(ArrayList<ColoredPoint> pointsWhite,ColoredPoint point,int edge){
+		HashSet<ColoredPoint> result = new HashSet<>();
+		
+		ArrayList<ColoredPoint> v = voisins(pointsWhite,point,edge);
+		for(ColoredPoint p : v) {
+			for(ColoredPoint q : pointsWhite) {
+				if(p.equals(q))continue;
+				if(p.distance(q)<edge)
+					result.add(p);
+			}
+		}
+		return new ArrayList<>(result);
+		
+	}
+
+	public Point plusProche(ArrayList<Point> points, Point p, int edge ) {
 		  double d=10000;
 		  
 		  Point pp=null;
@@ -121,7 +211,28 @@ public class Esclave implements Callable<ArrayList<Point>> {
 	  }
 	  
 	  
-	  public ArrayList<Point> voisins(ArrayList<Point> points, Point p, int edge) {
+	  public ArrayList<ColoredPoint> voisins(ArrayList<ColoredPoint> points, ColoredPoint p, int edge) {
+			ArrayList<ColoredPoint> voisins = new ArrayList<>();
+			for (ColoredPoint q : points) {
+				if (p.equals(q))
+					continue;
+				if (p.distance(q) < edge)
+					voisins.add(q);
+			}
+
+			return voisins;
+		}
+	  
+	  public ArrayList<Point> seul(ArrayList<Point> points,int edge){
+		  ArrayList<Point> seuls = new ArrayList<>();
+		  for(Point p : points) {
+			  if(voisinsP(points, p, edge).size()<1)
+				  seuls.add(p);
+		  }
+		  return seuls;
+	  }
+	  
+	  public ArrayList<Point> voisinsP(ArrayList<Point> points, Point p, int edge) {
 			int s = 0;
 			ArrayList<Point> voisins = new ArrayList<>();
 			for (Point q : points) {
@@ -134,4 +245,17 @@ public class Esclave implements Callable<ArrayList<Point>> {
 			return voisins;
 		}
 	  
+	  public ColoredPoint max(ArrayList<ColoredPoint> points,int edge) {
+		  
+		  int s=voisins(points,points.get(0),edge).size();
+		  ColoredPoint pp=points.get(0);
+		  for(ColoredPoint p : points) {
+			  if(voisins(points, p, edge).size()>s) {
+				  s=voisins(points,p,edge).size();
+				  pp=p;
+			  }
+		  }
+		  
+		  return pp;
+	  }
 }
